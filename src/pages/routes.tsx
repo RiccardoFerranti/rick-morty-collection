c7ac067f5d172a00a0bc9246f08561c0679e745c
Riccardo Ferranti <riccardoferranti@MacBook-Pro-di-Riccardo.local>
2022-10-24 17:50:35 +0200

    fix 404 routing for github pages

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Listing from './Listing';
 import Details from './Details';
@@ -9,11 +9,13 @@ import Layout from '../Layout/Layout';
 const AppRoutes: FC = () => (
   <Layout>
     <Routes>
-      <Route path="*" element={<NotFound />} />
       <Route path="/rick-morty-collection" element={<Listing />} />
+      <Route path='/' element={<Navigate replace to='/rick-morty-collection'/>} />
       <Route path="rick-morty-collection/character/:id" element={<Details />} />
       <Route path="rick-morty-collection/episode/:id" element={<Details />} />
       <Route path="rick-morty-collection/location/:id" element={<Details />} />
+      <Route path='/404' element={<NotFound/>} />
+      <Route path='*' element={<Navigate replace to='/404'/>} />
     </Routes>
   </Layout>
 );
